27d87eb18225645b4581cdb1224ba06dcfc205dc
lab661 <lab661@PC-445.urel.feec.vutbr.cz>
2020-02-26 10:49:33 +0100

    Lab04

---
+++ Labs/04-adder/binary_adder/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/lab661/Documents/xmastn02/Digital-electronics-1/Labs/04-adder/binary_adder/full_adderTB_isim_beh.exe" -prj "/home/lab661/Documents/xmastn02/Digital-electronics-1/Labs/04-adder/binary_adder/full_adderTB_beh.prj" "work.full_adderTB" 
